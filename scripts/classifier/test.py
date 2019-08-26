eb609248a498f1ad297e237708ee155ea902a128
Chan Young Song <chanyoungs@gmail.com>
2019-08-26 13:11:39 +0900

    Added timer on classifier

--- scripts/classifier/test.py
+++ scripts/classifier/test.py
@@ -1,5 +1,6 @@
 import numpy as np
 from classifier import Predict
+import time
 
 # Test run
 imgs = np.load("../../outputs/data340_v1_bs16/predictions/x_val.npy")
@@ -8,5 +9,6 @@ test = Predict(model_name="340", model_type="loss")
 
 while True:
     ind = int(input(f"Choose image index(0~{imgs.shape[0]}): "))
-    print(labels[ind])
+    start_time = time.time()
     test.predict(img=imgs[ind]/255, show_image=True, print_details=True, label=labels[ind])
+    print(f"Prediction took {time.time() - start_time:.4f}s")
