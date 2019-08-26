baebda157e607cf69da18e1b3e0bc2840ecf4a97
Chan Young Song <chanyoungs@gmail.com>
2019-08-26 17:44:44 +0900

    Moved to different drive

--- scripts/trainer/resnet/load_data.py
+++ scripts/trainer/resnet/load_data.py
@@ -8,7 +8,10 @@ def load_data(root, path, data):
     y = np.load(os.path.join(root, "data", f"labels_{data}x{data}.npy"))
 
     # Shuffling and splitting data into train, validation, test sets
-    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, shuffle=True, random_state=33)
+    # x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, shuffle=True, random_state=33)
+    # x_val, x_test, y_val, y_test = train_test_split(x_test, y_test, test_size=0.5, shuffle=True, random_state=33)
+
+    x_train, x_test, y_train, y_test = train_test_split(x, y, train_size=3000, shuffle=True, random_state=33)
     x_val, x_test, y_val, y_test = train_test_split(x_test, y_test, test_size=0.5, shuffle=True, random_state=33)
 
     # Save validation set
